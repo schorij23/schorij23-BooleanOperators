0f383c8aaa4ba5654266ddef1ae6b8935587c032
schorij23 <j_schori@aol.com>
2023-02-23 18:07:27 +0000

    labbbbbbbbbbb

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -14,7 +14,8 @@ public class Lab {
      * @return if b is true, return true. if b is false, return false.
      */
     public boolean returnBoolean(boolean b){
-        return false;
+
+        return b;
     }
 
     /**
@@ -23,7 +24,7 @@ public class Lab {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnNot(boolean b){
-        return false;
+        return !b;
     }
 
     /**
@@ -33,7 +34,11 @@ public class Lab {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnAnd(boolean b1, boolean b2){
-        return false;
+        //b1 = true;
+        //b2 = true;
+        //boolean bool = b1 && b2;
+    
+        return b1;
     }
 
     /**
@@ -43,7 +48,10 @@ public class Lab {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnOr(boolean b1, boolean b2){
-        return false;
+        //b1 = true;
+        //b1 = false;
+        //boolean bool = b1 || b1;
+      return b1;
     }
 
 
